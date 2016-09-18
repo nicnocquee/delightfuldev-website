8849231a41684a957759456f0457859e5ff50c2e
Nico Prananta <nicnocquee@users.noreply.github.com>
2016-09-18 16:12:00 +0200

    landing page

---
+++ src/features.js
@@ -0,0 +1,23 @@
+import professional from './professional.png'
+import high from './high.png'
+import rocket from './rocket.png'
+
+const features = [
+  {
+    image: professional,
+    title: "Professional Development",
+    body: "Let's be honest. The number of people who can make apps grow rapidly recently. However, creating high quality, easy to configure, and testable apps requires long and varied experiences. We have been developing iOS apps since 2010 and other apps since 2008."
+  },
+  {
+    image: high,
+    title: "High Quality",
+    body: "The iOS apps we make are not only following Apple user interface guidelines, we also follow the design trend in high quality apps in the App Store. But high quality app is not only about good looking user interface. It's also about great user experience. If it takes three taps or more to do something that user does frequently, there's something wrong with the app's usability. And we understand that."
+  },
+  {
+    image: rocket,
+    title: "Comprehensive Testing",
+    body: "Test is a first class citizen in our development process. Here's a secret. Many apps don't go through thorough testing process. Because testing is not a trivial process. But testing is critical. It can prevent crashes or even breaking the app in future updates."
+  }
+]
+
+export default features
