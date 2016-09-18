66f557623f3e9caa753f1c9d40041ba7eb8130ab
Nico Prananta <nicnocquee@users.noreply.github.com>
2016-09-19 00:29:33 +0200

    update script

--- update.sh
+++ update.sh
@@ -2,9 +2,10 @@
 npm run build
 git checkout -B gh-pages
 git add -f build
+git commit -am "Rebuild website"
+git filter-branch -f --prune-empty --subdirectory-filter build
 echo "www.delightfuldev.com" > CNAME
 git add -f CNAME
 git commit -am "Rebuild website"
-git filter-branch -f --prune-empty --subdirectory-filter build
 git push -f origin gh-pages
 git checkout -
