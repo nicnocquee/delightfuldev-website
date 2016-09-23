6f3eb3dd0c9c3485b5d23e44deece8d97e379e32
Nico Prananta <nicnocquee@users.noreply.github.com>
2016-09-24 00:32:59 +0200

    update instruction

--- update.sh
+++ update.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 npm run build
-cp ./src/delightfuldev-facebook-cover.png ./build/static/media/delightfuldev-facebook-cover.png
-git checkout -B gh-pages
-git add -f build
-git commit -am "Rebuild website"
-git filter-branch -f --prune-empty --subdirectory-filter build
-echo "www.delightfuldev.com" > CNAME
-git add -f CNAME
-git commit -am "Rebuild website"
-git push -f origin gh-pages
-git checkout -
+# cp ./src/delightfuldev-facebook-cover.png ./build/static/media/delightfuldev-facebook-cover.png
+# git checkout -B gh-pages
+# git add -f build
+# git commit -am "Rebuild website"
+# git filter-branch -f --prune-empty --subdirectory-filter build
+# echo "www.delightfuldev.com" > CNAME
+# git add -f CNAME
+# git commit -am "Rebuild website"
+# git push -f origin gh-pages
+# git checkout -
+
+scp -r build/ <username>@delightfuldev.com:/home/<username>/html
+echo "Now log in to server, then sudo cp -R html/ /var/www/ && rm -rf html"
